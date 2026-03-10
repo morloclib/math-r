eb4f220af9fa1ec38f4f65f2520e16f2385ecd19
Zebulun Arendsee <zbwrnz@gmail.com>
2026-03-09 23:05:22 -0400

    Release v0.1.0

---
+++ R/core.R
@@ -0,0 +1,59 @@
+# Constants
+
+morloc_pi <- function() pi
+
+morloc_e <- function() exp(1)
+
+# Trigonometric
+
+morloc_sin <- function(x) sin(x)
+
+morloc_cos <- function(x) cos(x)
+
+morloc_tan <- function(x) tan(x)
+
+morloc_asin <- function(x) asin(x)
+
+morloc_acos <- function(x) acos(x)
+
+morloc_atan <- function(x) atan(x)
+
+morloc_atan2 <- function(y, x) atan2(y, x)
+
+# Hyperbolic
+
+morloc_sinh <- function(x) sinh(x)
+
+morloc_cosh <- function(x) cosh(x)
+
+morloc_tanh <- function(x) tanh(x)
+
+morloc_asinh <- function(x) asinh(x)
+
+morloc_acosh <- function(x) acosh(x)
+
+morloc_atanh <- function(x) atanh(x)
+
+# Exponential and logarithmic
+
+morloc_exp <- function(x) exp(x)
+
+morloc_log2 <- function(x) log2(x)
+
+morloc_log10 <- function(x) log10(x)
+
+# Roots
+
+morloc_sqrt <- function(x) sqrt(x)
+
+# Rounding
+
+morloc_round <- function(x) round(x)
+
+morloc_ceil <- function(x) ceiling(x)
+
+morloc_floor <- function(x) floor(x)
+
+# Sign
+
+morloc_sign <- function(x) sign(x)
